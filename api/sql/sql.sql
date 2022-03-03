b06aa03815838cada95ced9548ecc3e14ccf1e11
alexandremsnunes <msn.alexandre@gmail.com>
2022-03-03 16:25:16 -0300

    User's model

---
+++ api/sql/sql.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+USE devbook;
+
+DROP TABLE IF EXISTS publications;
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    password varchar(100) not null,
+    createdIn timestamp default current_timestamp()
+) ENGINE=INNODB;
+
+CREATE TABLE followers(
+    user_id int not null,
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    follower_id int not null,
+    FOREIGN KEY (follower_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    primary key(user_id, follower_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publications(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+
+    autor_id int not null,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp
+) ENGINE=INNODB;
